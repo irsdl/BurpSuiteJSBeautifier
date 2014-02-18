4933e5b6e4063efdaa189e6d07d528692d797772
Soroush Dalili <s.d.github@gmail.com>
2014-02-18 23:46:36 +0000

    Update

--- jsbeautifier/src/burp/JSBeautifier/BeautifierPreferences.java
+++ jsbeautifier/src/burp/JSBeautifier/BeautifierPreferences.java
@@ -6,7 +6,7 @@ public class BeautifierPreferences {
 	private static final double version = 1.1;
 	private static final String appName = "Burp Suite JSBeautifier";
 	private static final String author = "Soroush Dalili (@irsdl)";
-	private static final String authorLink = "http://secproject.com/blog/";
+	private static final String authorLink = "https://secproject.com/";
 	private static final String projectLink = "https://github.com/irsdl/BurpSuiteJSBeautifier";
 	
 	public synchronized static double getVersionx() {
